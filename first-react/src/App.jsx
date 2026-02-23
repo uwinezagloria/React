2401daea127243e9d0dcc4ad8333f06585be9b3b
uwinezagloria <gloria7wineza@gmail.com>
2026-02-23 23:25:16 +0200

    react props

--- first-react/src/App.jsx
+++ first-react/src/App.jsx
@@ -1,13 +1,50 @@
-import Page from "./components/Parent-ChildComponents.jsx"
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
+import data from "./components/TravelJournalProject/data.js"
+
+import Page from "./components/Parent-ChildComponents.jsx";
 import NavBar from "./components/ReactFactsProject/navbar.jsx";
 import Main from "./components/ReactFactsProject/mainComponent.jsx";
-function App(){
+import Header from "./components/TravelJournalProject/Header.jsx";
+import Entry from "./components/TravelJournalProject/Entry.jsx";
+function App() {
+  const dataArray=data.map((item)=>{
+    return (
+    <Entry 
+    key={item.id}
+    {...item}
+
+    /*
+    key={item.id}
+     img={item.img} 
+     country={item.country} 
+     title={item.title}
+      googleMapsLink={item.googleMapsLink}
+      dates={item.dates}
+      text={item.text} */
+
+    
+     />
+    )
+})
   return (
     <>
-     {/*<Page></Page>*/}
+      {/*<Page></Page>*/}
+      {/* react fact project
      <NavBar />
-     <Main />
+     <Main /> */}
+
+      <Header />
+      <main>{dataArray}</main>
+      
     </>
-  )
+  );
 }
-export default App
+export default App;
---
+++ first-react/src/components/TravelJournalProject/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <header className="travel-header">
+            
+                <img src="./src/assets/Globe.png" alt="globe" />
+                 <p>my travel journal.</p>
+            
+        
+        </header>
+        
+    )
+}
