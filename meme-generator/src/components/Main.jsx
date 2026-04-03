753ed51b09ba810a629a775cba72cffe3ce93236
uwinezagloria <gloria7wineza@gmail.com>
2026-04-03 18:40:02 +0200

    meme-generator

---
+++ meme-generator/src/components/Main.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+
+export default function Main() {
+    /**
+     * Challenge: move the hardcoded meme info into React
+     * state. Use an object with `topText`, `bottomText`,
+     * and `imageUrl` properties, and set the initial values to
+     * the ones hardcoded below.
+     */
+    const [meme,setMeme]=useState({
+        topText:"One does not simply",
+        bottomText:"Walk into Mordor",
+        imageUrl:"http://i.imgflip.com/1bij.jpg"
+    })
+     /**
+         * Challenge: update the topText value in the meme state
+         * object every time the topText input box is changed
+         * 
+         * Note: don't worry about bottomText at this point.
+         */
+        const handleChange=(event)=>{
+            console.log(event)
+            const {value,name}=event.currentTarget
+            setMeme((prev)=>({
+                ...prev,
+                [name]:value
+            }))
+        }
+    
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
